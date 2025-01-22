bbb7532725bf9b94336291f4bcd1644b4c6335f1
Moli-X <327660509@qq.com>
2025-01-22 13:28:38 +0800

    Site updated: 2025-01-22 13:28:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/21/hello-world/"];function toRandomPost(){
+var posts=["2025/01/22/这是一篇新的博文/","2025/01/22/这是一篇HMI的博文/","2025/01/22/这是一篇机械手的文章/","2025/01/21/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
