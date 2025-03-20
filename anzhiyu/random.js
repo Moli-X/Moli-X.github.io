f4f10984e309b9d111fe1c6e31fba2ac0ccfa8d8
Moli-X <327660509@qq.com>
2025-03-20 22:16:55 +0800

    Site updated: 2025-03-20 22:16:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/22/article/theme/安知鱼主题教程/","2025/03/17/article/theme/实现3D效果教程/","2025/03/13/article/automation/HMI/HMI触摸屏程序设计详解/","2025/03/13/article/automation/controller/凸轮表生成功能块实现详解/","2025/03/13/article/automation/scara/工业机器人编程指南/","2025/01/22/article/automation/HMI/台达触摸屏在工业自动化中的应用/","2025/01/22/article/automation/controller/色标补偿功能块实现详解/"];function toRandomPost(){
+var posts=["2025/01/22/article/theme/安知鱼主题教程/","2025/03/17/article/theme/实现3D效果教程/","2025/03/13/article/automation/HMI/HMI触摸屏程序设计详解/","2025/03/13/article/automation/controller/凸轮表生成功能块实现详解/","2025/03/13/article/automation/scara/工业机器人编程指南/","2025/01/22/article/automation/controller/色标补偿功能块实现详解/","2025/01/22/article/automation/HMI/台达触摸屏在工业自动化中的应用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
